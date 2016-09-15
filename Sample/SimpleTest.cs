d788d8723b53d440926cc539df95fb71ece42463
rolandh <roland.c.harrison@gmail.com>
2016-09-15 17:19:38 +0930

    Removed old licenses that are not applicable

--- Sample/SimpleTest.cs
+++ Sample/SimpleTest.cs
@@ -1,30 +1,4 @@
-﻿#region Copyright (c) 2010, Michael Kelly
-/* 
- * Copyright (c) 2010, Michael Kelly
- * michael.e.kelly@gmail.com
- * http://michael-kelly.com/
- * 
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
- * Neither the name of the organization nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- */
-#endregion License
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Windows.Forms;
@@ -36,7 +10,6 @@ namespace Sample
     using J2534DotNet.Logger;
     using System.Runtime.InteropServices;
     using System.Threading;
-    //using J2534DotNet.Logger;
 
     public partial class SimpleTest : Form
     {
@@ -319,13 +292,16 @@ namespace Sample
 
                 //Disconnect();
                 return;
+            }  else
+            {
+                MessageBox.Show(String.Format("Successfull entered level 1 security mode!");
             }
 
             //Download the PCM now
-            byte[] bytes;
-            UpdateLog("ReadMemoryByAddress");
+            //byte[] bytes;
+            //UpdateLog("ReadMemoryByAddress");
 
-            bool success = comm.ReadMemoryByAddress(0, out bytes);
+            //bool success = comm.ReadMemoryByAddress(0, out bytes);
 
             //comm.Disconnect();
 
